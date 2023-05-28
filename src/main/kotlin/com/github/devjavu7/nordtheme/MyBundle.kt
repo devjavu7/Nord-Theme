f9012cc2d4b02d8f9c992f1242415080fed29276
GitHub Action <action@github.com>
2023-05-28 14:07:21 +0000

    Template cleanup

--- src/main/kotlin/com/github/devjavu7/nordtheme/MyBundle.kt
+++ src/main/kotlin/com/github/devjavu7/nordtheme/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.devjavu7.nordtheme
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/devjavu7/nordtheme/listeners/MyFrameStateListener.kt
+++ src/main/kotlin/com/github/devjavu7/nordtheme/listeners/MyFrameStateListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.devjavu7.nordtheme.listeners
 
 import com.intellij.ide.FrameStateListener
 import com.intellij.openapi.diagnostic.thisLogger
